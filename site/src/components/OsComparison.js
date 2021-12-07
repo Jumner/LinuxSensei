9c79ef4fb09d556476fe4b42e8a09b39bda466dc
Curtis <>
2021-12-06 20:20:58 -0500

    OS Comparison

--- site/src/components/OsComparison.js
+++ site/src/components/OsComparison.js
@@ -3,9 +3,36 @@ import Os from "./Os";
 export default function OsComparison() {
 	return (
 		<HStack w="80vw" justify="space-between">
-			<Os name="Windows" items={["a", "b", "c"]} />
-			<Os name="Linux" items={["a", "b", "c"]} />
-			<Os name="Mac Os" items={["a", "b", "c"]} />
+			<Os
+				name="Windows"
+				items={[
+					"Created by Microsoft",
+					"Found on most PC products, but available on other devices using software",
+					"Used often for personal devices, also sees widespread use in workplace",
+					"Computer gaming revolves almost entirely around windows",
+					"Recently, Windows has been criticized for their boxy and bland ui.",
+				]}
+			/>
+			<Os
+				name="Linux"
+				items={[
+					"Created by Linus Torvaldis, currently devloped by thousands of developers worldwide",
+					"Free",
+					"Widely used for security, servers, smart devices, internet of things",
+					"Open source is actually disadvantageous for linux in terms of gaming",
+					"Bare bones without any configuration, but highly customizable.",
+				]}
+			/>
+			<Os
+				name="Mac Os"
+				items={[
+					"Created by Apple",
+					"Free",
+					"Mostly found on Apple devices, but technically available anywhere using software",
+					"Apple products have hardware capable of playing games, but few games are ported to Mac systems",
+					" Visual flair and ui design make MacOS appealing for casual tech users",
+				]}
+			/>
 		</HStack>
 	);
 }
