dc03a2ab1a2be39688ee6f83a16afa757615dd1a
JumnerArch <1frankjus@hdsb.ca>
2021-12-08 23:14:10 -0500

    added images

--- site/pages/index.js
+++ site/pages/index.js
@@ -3,18 +3,32 @@ import Home1 from "../src/components/home";
 import Footer from "../src/components/Footer";
 import Section from "../src/components/Section";
 import { Flex } from "@chakra-ui/layout";
+
+import GraphicalInstall from "./img/GraphicalInstall.png";
+import hostname from "./img/hostname.png";
+import root from "./img/root.png";
+import users from "./img/users.png";
+import partition from "./img/partition.png";
+import partition2 from "./img/partition2.png";
+import media from "./img/media.png";
+import mirror from "./img/mirror.png";
+import proxy from "./img/proxy.png";
+import popularity from "./img/popularity.png";
+import software from "./img/software.png";
+import grub from "./img/grub.png";
+
 export default function Home() {
 	return (
 		<Flex justify="space-between" direction="column">
 			<Nav />
 			<Home1 />
 			<Section heading="Installation Guide">
-				<Section heading="Obtaining a Bootable Usb">
+				<Section heading="Obtaining a Bootable Usb" img={GraphicalInstall}>
 					Obtain a bootable usb with a linux debian installer on it. Once
 					inserting it in the computer, the installer menu will start up and you
 					can select your method of installation.
 				</Section>
-				<Section heading="Select Graphical Install">
+				<Section heading="Select Graphical Install" img={hostname}>
 					After clicking “graphical install”, you will be brought to a page
 					where you can select your desired language, your location, and
 					keyboard type. You’ll then need to enter the hostname of the system.
@@ -25,7 +39,7 @@ export default function Home() {
 					You may then be prompted to enter a domain name. It’s perfectly fine
 					to leave this blank and move on.
 				</Section>
-				<Section heading="Root Account Creation">
+				<Section heading="Root Account Creation" img={root}>
 					The next step of the process is to set up a root account. Essentially,
 					this is an account with full administrative privileges, and access to
 					all commands and files on the system. It is important that you have
@@ -33,7 +47,7 @@ export default function Home() {
 					you should write down the username and password of the root account
 					and keep it somewhere safe.
 				</Section>
-				<Section heading="User Account Creation & Time Zone">
+				<Section heading="User Account Creation & Time Zone" img={users}>
 					Afterwards, a prompt for the user’s real name and chosen password will
 					be displayed. Immediately after, a username. Similar to the root
 					account, it is essential to keep this information written somewhere
@@ -42,14 +56,14 @@ export default function Home() {
 					uppercase letters to the username will halt your progress, lowercase
 					letters only. Enter your time zone next.
 				</Section>
-				<Section heading="Partioning">
+				<Section heading="Partioning" img={partition}>
 					you will be prompted to partition a disk. This step is done to
 					designate a specific space on a disk for your install to be located.
 					If you are unaware of how to partition a disk, the installer offers a
 					guide. This will outline the process and make recommendations for how
 					to partition.
 				</Section>
-				<Section heading="Partioning Scheme">
+				<Section heading="Partioning Scheme" img={partition2}>
 					Debian offers different schemes for partitioning, where you are able
 					to separate things into multiple different disks. For beginners, we
 					recommend that everything stay on the same disk for a simpler setup,
@@ -58,32 +72,32 @@ export default function Home() {
 					finally hit confirm when the installer asks permission to complete the
 					partitioning.
 				</Section>
-				<Section heading="Additional Installation">
+				<Section heading="Additional Installation" img={media}>
 					After, you will be prompted to install anything else, but it’s best to
 					save customization for later, and there is nothing else to install, so
 					this process can be skipped.
 				</Section>
-				<Section heading="Debian Archive">
+				<Section heading="Debian Archive" img={mirror}>
 					Time to select the Debian Archive Country and Mirror. These consist of
 					your country and the suggested domain for it. These decide where
 					debian is installed from. After choosing the country, it is best to
 					select the recommended archive mirror, for Canada, this was
 					deb.debian.org.
 				</Section>
-				<Section heading="HTTP Proxy">
+				<Section heading="HTTP Proxy" img={proxy}>
 					Afterwards, there is the option to enter an HTTP proxy, an external
 					connection to install through. This is a middleman that can be helpful
 					in bypassing any restrictions that may come up depending on the
 					network. For new users without any restrictions, this can be left
 					blank
 				</Section>
-				<Section heading="Popularity Contest">
+				<Section heading="Popularity Contest" img={popularity}>
 					Next is what’s called the Popularity Contest. This is no social event,
 					just an ask if your data can be used for developers to better the
 					experience. This decision will not impact the experience at all, so it
 					is all up to the user.
 				</Section>
-				<Section heading="Additional Software">
+				<Section heading="Additional Software" img={software}>
 					Approaching the end, there is a checklist with names of software
 					available to install. A few will be already selected, and these are
 					basics. Everything will run perfectly fine with the default
@@ -91,7 +105,7 @@ export default function Home() {
 					managers, desktop environments and other components. It is recommended
 					to just continue with the already selected elements.
 				</Section>
-				<Section heading="Grub Boot Loader">
+				<Section heading="Grub Boot Loader" img={grub}>
 					Asking to install the GRUB boot loader. This is a must, as this is
 					what actually starts the OS. Once again, it will ask for a device for
 					something to install, and there will be a drive selected on default.
--- site/src/components/Section.js
+++ site/src/components/Section.js
@@ -1,11 +1,13 @@
-import { Divider, Heading, Flex, Container } from "@chakra-ui/react";
+import { Divider, Heading, Flex, Container, Image } from "@chakra-ui/react";
 
-export default function Section({ children, heading }) {
+export default function Section({ children, heading, img }) {
+	let image = img ? <Image src={img.src} alt="Image not found" m="5" /> : "";
 	return (
 		<Flex justify="space-evenly" direction="column" align="center">
 			<Divider my="10" w="95vw" />
 			<Heading my="5">{heading}</Heading>
 			<Container>{children}</Container>
+			{image}
 		</Flex>
 	);
 }
