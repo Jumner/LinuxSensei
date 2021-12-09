2817e3f347d9e617890c268a5f1f3cc126420278
JumnerArch <1frankjus@hdsb.ca>
2021-12-08 22:48:40 -0500

    cleaned up the sections

--- site/src/components/Section.js
+++ site/src/components/Section.js
@@ -1,11 +1,11 @@
-import { Divider, Heading, Flex } from "@chakra-ui/react";
+import { Divider, Heading, Flex, Container } from "@chakra-ui/react";
 
 export default function Section({ children, heading }) {
 	return (
 		<Flex justify="space-evenly" direction="column" align="center">
 			<Divider my="10" w="95vw" />
 			<Heading my="5">{heading}</Heading>
-			{children}
+			<Container>{children}</Container>
 		</Flex>
 	);
 }
