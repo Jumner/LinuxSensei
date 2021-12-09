1c6826496d8d8db15d4e00713ba114538547c2ed
JumnerArch <1frankjus@hdsb.ca>
2021-12-08 23:38:32 -0500

    added cheat sheet

--- site/pages/resources.js
+++ site/pages/resources.js
@@ -1,10 +1,13 @@
-import { Flex, HStack } from "@chakra-ui/layout";
+import { Center, Flex, HStack, Text } from "@chakra-ui/layout";
 import Faq from "../src/components/Faq";
 import Nav from "../src/components/Nav";
 import Footer from "../src/components/Footer";
 import Section from "../src/components/Section";
 import OsComparison from "../src/components/OsComparison";
 import DistroComparison from "../src/components/DistroComparison";
+
+import cheatsheet from "./img/cheatsheet.png";
+
 export default function resources() {
 	return (
 		<Flex justify="space-between" direction="column">
@@ -17,6 +20,19 @@ export default function resources() {
 			</Section>
 			<Section heading="Distro Comparison">
 				<DistroComparison />
+			</Section>
+			<Section heading="The Linux Cheat sheet" img={cheatsheet}>
+				<Center>
+					<Text
+						as="a"
+						href={cheatsheet.src}
+						color="main.400"
+						textDecor="underline"
+						fontWeight="bolder"
+					>
+						Here is a direct link to the image
+					</Text>
+				</Center>
 			</Section>
 			<Footer />
 		</Flex>
--- site/src/components/Section.js
+++ site/src/components/Section.js
@@ -1,7 +1,17 @@
 import { Divider, Heading, Flex, Box, Image } from "@chakra-ui/react";
 
 export default function Section({ children, heading, img }) {
-	let image = img ? <Image src={img.src} alt="Image not found" m="5" /> : "";
+	let image = img ? (
+		<Image
+			src={img.src}
+			alt="Image not found"
+			m="5"
+			borderRadius="15"
+			boxShadow="md"
+		/>
+	) : (
+		""
+	);
 	return (
 		<Flex justify="space-evenly" direction="column" align="center">
 			<Divider my="10" w="95vw" />
